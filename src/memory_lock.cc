7c89789dd0642e6f6eb5c5bada31a2b8d6ed9c15
Matthias Bühlmann <matthias.buehlmann@mabulous.com>
2020-08-08 23:24:30 +0200

    Corrected and extended defines.

--- src/memory_lock.cc
+++ src/memory_lock.cc
@@ -1,11 +1,18 @@
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+  #if !defined(_WIN32_WINNT) || (_WIN32_WINNT < 0x0602)
+    #pragma message ( "To use PrefetchMemory(), compile with _WIN32_WINNT at least 0x602 (aka Windows 8)." )
+  #else
+    #define WINPREFETCHMEMORY 1
+  #endif
 #include <windows.h>
 #include <memoryapi.h>
-#endif
-#ifdef linux
+#define WINMEM 1
+#elif defined(__linux__) || defined(__unix__)
 #include <unistd.h>
+#define UNIXMEM 1
 #endif
-#include <exception>
+#include <stdexcept>
+#include <string>
 #include "memory_lock.h"
 
 namespace {
@@ -22,53 +29,60 @@ constexpr T AlignDown(T value, T align) { return value & ~(align - 1); }
 template <typename T>
 constexpr T AlignUp(T value, T align) { return (value + (align - 1)) & ~(align - 1); }
 
+std::string LastSystemErrorText() {
+  char err[256];
+  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), err, 255, NULL);
+  return std::string(err);
+}
+
 }
 
 namespace memorylock {
 
 void LockMemory(void* addr, size_t len) {
-#if defined(_unix_)
+#if defined(UNIXMEM)
   const size_t pageSize = GetPageSize();
   if (mlock(AlignDown(addr, pageSize), AlignUp(len, pageSize)))                                                                                     
-    throw std::exception(LastSystemErrorText());
-#elif defined(_win_)
+    throw std::runtime_error(LastSystemErrorText());
+#elif defined(WINMEM)
   HANDLE hndl = GetCurrentProcess();
   size_t min, max;
   if (!GetProcessWorkingSetSize(hndl, &min, &max))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
   if (!SetProcessWorkingSetSize(hndl, min + len, max + len))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
   if (!VirtualLock(addr, len))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
 #endif
 }
 
 void UnlockMemory(void* addr, size_t len) {
-#if defined(_unix_)
+#if defined(UNIXMEM)
   const size_t pageSize = GetPageSize();
   if (munlock(AlignDown(addr, pageSize), AlignUp(len, pageSize)))                                                                                     
-    throw std::exception(LastSystemErrorText());
-#elif defined(_win_)
+    throw std::runtime_error(LastSystemErrorText());
+#elif defined(WINMEM)
   HANDLE hndl = GetCurrentProcess();
   if (!VirtualLock(addr, len))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
   size_t min, max;
   if (!GetProcessWorkingSetSize(hndl, &min, &max))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
   if (!SetProcessWorkingSetSize(hndl, min - len, max - len))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
 #endif
 }
 
 void PrefetchMemory(MemoryRange* ranges, size_t elements) {
-#if defined(_unix_)
+#if defined(UNIXMEM)
   // TODO
-#elif defined(_win_)
+#elif defined(WINPREFETCHMEMORY)
   HANDLE hndl = GetCurrentProcess();
   ULONG_PTR n = elements;
-  PWIN32_MEMORY_RANGE_ENTRY* entries = ranges;
+  WIN32_MEMORY_RANGE_ENTRY* entries = (WIN32_MEMORY_RANGE_ENTRY*)ranges;
   if (!PrefetchVirtualMemory(hndl, n, entries, 0))
-    throw std::exception(LastSystemErrorText());
+    throw std::runtime_error(LastSystemErrorText());
 #endif
 }
 
